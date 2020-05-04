49e4f3c153239d4cc088fd956b47a261e6438dd1
hernan232 <hdvanegasm@gmail.com>
2020-05-04 17:30:49 -0500

    Steps per epoch

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ def transform_image(screen):
 
     
 def conv2d_size_out(size, kernel_size = 5, stride = 2):
-            return (size - (kernel_size - 1) - 1) // stride  + 1
+            return (size - (kernel_size - 1) - 1) // stride + 1
 
 
 def process_state(cumulative_screenshot):
